606b92d4f0d8d75649271ef82c000b564c66747c
jmpala <juanmartinpalazzolo@icloud.com>
2025-08-03 21:44:39 +0200

    - updated migration with missing status value

--- migrations/1-db-creation.sql
+++ migrations/1-db-creation.sql
@@ -1,8 +1,12 @@
+USE todolist;
+
 DROP DATABASE IF EXISTS todolist;
 
 CREATE DATABASE todolist;
 
+DROP TABLE IF EXISTS tasks;
 CREATE TABLE tasks (
     id integer primary key auto_increment,
-    task varchar(255)
+    task varchar(255),
+    status varchar(255)
 );
